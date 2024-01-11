8afaa89b424a8d8d39b433e7f6bc8d7ae01f820c
mesutsemiz <semihturgutlu@gmail.com>
2024-01-11 23:56:22 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
+import Header from "./components/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+    <div className='alert alert-primary'>
+        <h2>Context`ten gelen veri</h2>
+    </div>
+
+    </div>
+  )
+}
+
+export default Header
