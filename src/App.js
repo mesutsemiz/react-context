31a408c231d3cbcd3bfebb49cacd3d51f735c7c0
mesutsemiz <semihturgutlu@gmail.com>
2024-01-12 21:54:43 +0100

    Menu page created

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./style.css";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { GlobalProvider } from "./Context/GlobalState";
+import Menu from "./components/Menu";
 
 export default function App() {
   return (
     <div className="container">
       <div className="row">
         <GlobalProvider>
+          <Menu/>
         <Header />
         </GlobalProvider>
       </div>
--- src/Context/GlobalState.js
+++ src/Context/GlobalState.js
@@ -4,7 +4,7 @@ export const GlbalContext = createContext();
 
 export const GlobalProvider=(props)=>{
     return(
-        <GlbalContext.Provider value={{deger:"mesut" }}>
+        <GlbalContext.Provider value={{id:25,deger:"mesut" }}>
             {props.children}
         </GlbalContext.Provider>
     )
